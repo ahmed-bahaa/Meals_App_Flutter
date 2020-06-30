83014505191629b1284404ec9c97df62767ffbab
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-06-30 23:34:10 +0200

    adding base date

---
+++ lib/dummy_data.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+import 'package:flutter/material.dart';
+
+var DYMMY = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Egyptian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Frensh',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Chinease',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Easy & Quick',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Pizza',
+    color: Colors.lime,
+  ),
+];
