21be5b112ccf870a7bf78db0d856554aca34f9cb
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-07-01 00:40:39 +0200

    changing the home page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,20 +8,26 @@ import './dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DYMMY
-          .map(
-            (catData) => CategoryItem(
-              catData.title,
-              catData.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeal"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        children: DYMMY
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
